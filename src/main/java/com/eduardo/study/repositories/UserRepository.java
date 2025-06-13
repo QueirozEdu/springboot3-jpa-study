36253f126a23dc01ce0fc8db3d5a6f60d17c3609
Eduardo Queiroz <eduhmq@gmail.com>
2025-06-13 20:58:33 -0300

    add dependency injection, database seeding and jpa repository

---
+++ src/main/java/com/eduardo/study/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.eduardo.study.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduardo.study.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+}
