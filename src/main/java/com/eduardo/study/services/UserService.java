5d17357769fb4c35445666e5e6c594a52b973f55
Eduardo Queiroz <eduhmq@gmail.com>
2025-06-13 22:18:57 -0300

    add find user by id and component registration

---
+++ src/main/java/com/eduardo/study/services/UserService.java
@@ -0,0 +1,26 @@
+package com.eduardo.study.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardo.study.entities.User;
+import com.eduardo.study.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
