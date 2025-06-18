3a11638e557c2e8e83506418406c620c3e955f4a
Eduardo Queiroz <eduhmq@gmail.com>
2025-06-17 22:16:06 -0300

    add Exception handler

--- src/main/java/com/eduardo/study/services/UserService.java
+++ src/main/java/com/eduardo/study/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.eduardo.study.entities.User;
 import com.eduardo.study.repositories.UserRepository;
+import com.eduardo.study.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
---
+++ src/main/java/com/eduardo/study/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.eduardo.study.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
